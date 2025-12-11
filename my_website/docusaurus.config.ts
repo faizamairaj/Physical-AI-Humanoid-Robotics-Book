301f95a554f5e4511f196e8d36c365d69fca3037
Faiza Mairaj <faizamairaj@gmail.com>
2025-12-11 18:51:30 +0500

    physical_robotics_book

--- my_website/docusaurus.config.ts
+++ my_website/docusaurus.config.ts
@@ -15,10 +15,10 @@ const config: Config = {
   },
 
   // Set the production url of your site here
-  url: 'https://physical-ai-humanoid-robotics-book.vercel.app',
+  url: 'https://faizamairaj.github.io',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/',
+  baseUrl: '/Physical-AI-Humanoid-Robotics-Book/',
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
